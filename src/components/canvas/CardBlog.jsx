3a0cb05e7d26b6111fcfa7470fb3886f5d41772e
Isaac Santos <isaac.emanuel@live.com>
2024-12-08 03:07:21 -0600

    +blogFolder minimum changes

--- src/components/canvas/CardBlog.jsx
+++ src/components/canvas/CardBlog.jsx
@@ -4,7 +4,7 @@ const CardBlog = ({ blog }) => {
 	return (
 		<div className='h-full w-[400px] m-2 flex-shrink-0 cursor-pointer'>
 			<div className='rounded-3xl overflow-hidden mb-4 relative h-[250px]'>
-				<img src={`/imgs/blogpost${blog.id}.avif`} alt={blog.title} />
+				<img src={`/blog/blogpost${blog.id}.avif`} alt={blog.title} />
 				<span className='absolute top-3 left-4 border border-blue-200 text-xs rounded-xl px-4 py-2 font-semibold capitalize bg-blue-100'>
 					{blog.category}
 				</span>
