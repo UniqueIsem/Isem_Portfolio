504db3e9a6a68e27d2694d2164de1d0113659e3b
Isaac Santos <isaac.emanuel@live.com>
2025-01-15 16:13:00 -0600

    +gh pages

--- src/components/canvas/CardBlog.jsx
+++ src/components/canvas/CardBlog.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 const CardBlog = ({ blog }) => {
 	return (
-		<div className='w-[400px] h-fit m-2 flex-shrink-0 cursor-pointer'>
+		<div className='w-[400px] h-fit m-2 flex-shrink-0'>
 			<div className='rounded-3xl overflow-hidden mb-4 relative h-[250px]'>
+				<div className='absolute p-10 bg-black  text-justify'>
+					<span className='text-wrap text-center text-xl'>
+						{blog.description}
+					</span>
+				</div>
 				<img src={blog.img} alt={blog.title} />
 				<span className='absolute top-3 left-4 border border-purple-950 text-xs rounded-xl px-4 py-2 font-semibold capitalize bg-gray-800'>
 					{blog.category}
 				</span>
+
 			</div>
 			<div className='px-4 flex gap-4'>
 				{/*<img
